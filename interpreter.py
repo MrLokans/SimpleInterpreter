bfa89824f06372434c66bd294ce75c22d27231ce
MrLokans <trikster1911@gmail.com>
2016-02-09 22:35:56 +0300

    Added subtraction, multiple digit numbers and whitespace ignoring.

--- interpreter.py
+++ interpreter.py
@@ -22,33 +22,49 @@ class Interpreter(object):
         self.text = text
         self.text_position = 0
         self.current_token = None
+        self.current_char = self.text[self.text_position]
+
+    def advance(self):
+        """Advance pointer to the current symbol and its value"""
+        self.text_position += 1
+        if self.text_position > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.text_position]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def consume_integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
 
     def get_next_token(self):
         """Tokenizer method, that splits text into token sequence"""
 
-        # If we encounter last symbol it means we reached the end of the text
-        if self.text_position > len(self.text) - 1:
-            return Token(EOF, None)
+        while self.current_char is not None:
 
-        current_char = self.text[self.text_position]
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-        # handle integer tokenization
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
-            self.text_position += 1
-            return token
-        if current_char == "+":
-            token = Token(PLUS, current_char)
-            self.text_position += 1
-            return token
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.consume_integer())
 
-        if current_char == "-":
-            token = Token(PLUS, current_char)
-            self.text_position += 1
-            return token
+            if self.current_char == "+":
+                self.advance()
+                return Token(PLUS, '+')
 
-        # if symbol is not identified - exit with error
-        raise InterpreterParsingError()
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            raise InterpreterParsingError()
+        return Token(EOF, None)
 
     def eat_token(self, token_type):
 
@@ -65,7 +81,10 @@ class Interpreter(object):
         self.eat_token(INTEGER)
 
         op = self.current_token
-        self.eat_token(PLUS)
+        if op.type == PLUS:
+            self.eat_token(PLUS)
+        else:
+            self.eat_token(MINUS)
 
         right = self.current_token
         self.eat_token(INTEGER)
@@ -73,8 +92,8 @@ class Interpreter(object):
         if op.type == MINUS:
             result = left.value - right.value
 
-        if op.type == PLUS:
-            result = left.value - right.value
+        else:
+            result = left.value + right.value
 
         return result
 
